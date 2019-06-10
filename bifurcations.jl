69a59c8e16320e678c2c820bc8b111df19876771
LeMinaw <leminaw@gmail.com>
2019-06-11 01:20:17 +0200

    added batch exemple + demo code cleanup

--- bifurcations.jl
+++ bifurcations.jl
@@ -59,59 +59,61 @@ module Bifurcations
     end
 
 
-    #=
-    function compute(y1::Real, y2::Real, x1::Real, x2::Real, i::Int)
-        println("=== STARTING COMPUTATION ===")
-        colormap = Gradient(RGB(1,0,0), RGB(0,0,0))
-        pop = Population(logistic, [id, cst(3.8)], .5)
-        plot(pop, colormap, name=format("u/{:.5f}-{:.5f}__{:.5f}-{:.5f}", y1, y2, x1, x2), iters=trunc(i), ymin=y1, ymax=y2, xmin=x1, xmax=x2)
-        println("Done.")
-    end
+    # Obsolete exemple for plotting animated zooms
 
-    # function compute(x::Real)
-    #     red2black = Gradient(RGB(1,0,0), RGB(0,0,0))
-    #     pop = Population(logistic, [id, cst(x)], .5)
-    #     plot(pop, red2black, format("i/{:.5f}", x))
-    # end
-
-    function expspace(start::Real, stop::Real, steps::Int)
-        logStart = log(start)
-        logStop  = log(stop)
-        space = linspace(logStart, logStop, steps)
-        exp.(space)
-    end
-
-    function zoom(lowInit, upInit, fact, frames)
-        lows, ups = [], []
-        low,  up  = lowInit, upInit
-        for n = 1:frames
-            push!(lows, low)
-            push!(ups,  up)
-            range = up - low
-            low += range * fact
-            up  -= range * fact
-        end
-        lows, ups
-    end
-
-    const FRAMES = 2000
-    const Y_LIMITS = zoom(0.99680, 4.00000, 0.025, FRAMES)
-    const X_LIMITS = zoom(0.00000, 0.82896, 0.02, FRAMES)
-
-    # crops a window [lower, upper] to make <center> its midpoint
-    function limits(lower, center, upper)
-        range = upper -lower
-        if center < range/2
-            return lower, 2 * center
-        elseif center > range/2
-            return 2 * center - range, upper
-        else
-            lower, upper
+    #= function __init__()
+        function compute(y1::Real, y2::Real, x1::Real, x2::Real, i::Int)
+            println("=== STARTING COMPUTATION ===")
+            colormap = Gradient(RGB(1,0,0), RGB(0,0,0))
+            pop = Population(logistic, [id, cst(3.8)], .5)
+            plot(pop, colormap, name=format("u/{:.5f}-{:.5f}__{:.5f}-{:.5f}", y1, y2, x1, x2), iters=trunc(i), ymin=y1, ymax=y2, xmin=x1, xmax=x2)
+            println("Done.")
         end
 
-    # pmap(compute, Y_LIMITS[1], Y_LIMITS[2], fill(0.4, FRAMES), fill(0.6, FRAMES), Int64.(trunc.(linspace(600, 6000, FRAMES))))
-    pmap(compute, Y_LIMITS[1], Y_LIMITS[2], X_LIMITS[1], X_LIMITS[2], Int64.(trunc.(linspace(600, 100000, FRAMES))))
-    =#
+        # function compute(x::Real)
+        #     red2black = Gradient(RGB(1,0,0), RGB(0,0,0))
+        #     pop = Population(logistic, [id, cst(x)], .5)
+        #     plot(pop, red2black, format("i/{:.5f}", x))
+        # end
+
+        function expspace(start::Real, stop::Real, steps::Int)
+            logStart = log(start)
+            logStop  = log(stop)
+            space = linspace(logStart, logStop, steps)
+            exp.(space)
+        end
+
+        function zoom(lowInit, upInit, fact, frames)
+            lows, ups = [], []
+            low,  up  = lowInit, upInit
+            for n = 1:frames
+                push!(lows, low)
+                push!(ups,  up)
+                range = up - low
+                low += range * fact
+                up  -= range * fact
+            end
+            lows, ups
+        end
+
+        const FRAMES = 2000
+        const Y_LIMITS = zoom(0.99680, 4.00000, 0.025, FRAMES)
+        const X_LIMITS = zoom(0.00000, 0.82896, 0.02, FRAMES)
+
+        # crops a window [lower, upper] to make <center> its midpoint
+        function limits(lower, center, upper)
+            range = upper -lower
+            if center < range/2
+                return lower, 2 * center
+            elseif center > range/2
+                return 2 * center - range, upper
+            else
+                lower, upper
+            end
+
+        # pmap(compute, Y_LIMITS[1], Y_LIMITS[2], fill(0.4, FRAMES), fill(0.6, FRAMES), Int64.(trunc.(linspace(600, 6000, FRAMES))))
+        pmap(compute, Y_LIMITS[1], Y_LIMITS[2], X_LIMITS[1], X_LIMITS[2], Int64.(trunc.(linspace(600, 100000, FRAMES))))
+    end =#
 
     function __init__()
         # pop = Population(logistic, [id, cst(3.9), cst(3.9), cst(3.9)], .5) # pas mal
@@ -119,23 +121,48 @@ module Bifurcations
         # pop = Population(logistic, [id, cst(3.85), cst(3.75)], .5) #vraiment cool
         pop = Population(logistic, [id, cst(3.8), cst(3.7)], .5)
 
-        colormap = Gradient(RGB(0,0,0), RGB(1,0,0))
-        # colormap = Gradient(RGB(1,0,0), RGB(1,1,1))
+        black = RGB(0,0,0)
+        red   = RGB(1,0,0)
+        white = RGB(1,1,1)
+        alpha = RGBA(1,1,1,0)
 
-        # plot(pop, colormap; name="precomp", sx=128, sy=128) # Precompilation
+        black2red = Gradient(black, red)
+        red2white = Gradient(red,   white)
+        black     = Gradient(black, black)
 
-        plot(pop, colormap;
-            # background = RGB(1,1,1),
-            # background = RGB(0,0,0),
-            # background = RGBA(0,0,0,0),
+
+        # Precomp func for benchmarks
+
+        # plot(pop, colormap; name="precomp", sx=128, sy=128)
+
+
+        # Usual demo func
+
+        #= plot(pop, black_to_red;
             name = string(rand(0000:9999)),
-            # iters = 3000,
-            iters = 600,
+            iters = 3000,
             osa = true,
-            # sx = 1080*4, sy = 1920*12,
-            sx = 1080, sy = 1920*3,
+            sx = 1080*2, sy = 1920*6,
             xmin = 0.0, xmax = 1.0,
             ymin = 0.0, ymax = 4.0
-        )
+        ) =#
+
+
+        # Batch multiple views
+        
+        n = 24
+        for i = n:n
+            println("Computing frame $i/$n...")
+
+            plot(pop, black2red;
+                name = format("{:02d}", i),
+                iters = 1000,
+                osa = true,
+                # sx = 297, sy = 420,
+                sx = 2970, sy = 4200,
+                xmin = 0.0, xmax = 1.0,
+                ymin = 4(i-1)/n, ymax = 4i/n
+            )
+        end
     end
 end
